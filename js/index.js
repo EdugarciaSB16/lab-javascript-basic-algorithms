261485ba1193b7efb65068e3b5eea5bc2dc2523d
monteromario <59968975+monteromario@users.noreply.github.com>
2020-11-13 17:03:55 +0100

    Done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 // Iteration 1: Names and Input
-let hacker1 = "Eduardo"
-let hacker2 = "Mario"
-console.log(`The driver's name is ${hacker1}`)
-console.log(`The navigator's name is ${hacker2}`)
+let hacker1 = "Eduardo";
+let hacker2 = "Mario";
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
@@ -16,8 +17,10 @@ if (hacker1.length > hacker2.length) {
 // Iteration 3: Loops
 let hacker1upper = hacker1.toUpperCase().split("").join(" ")
 console.log(hacker1upper)
+
 let hacker2reverse = hacker2.split("").reverse().join("")
 console.log(hacker2reverse)
+
 if (hacker1.localeCompare(hacker2) === -1) {
   console.log("The driver's name goes first.")
 } else if (hacker1.localeCompare(hacker2) === 1) {
@@ -26,11 +29,24 @@ if (hacker1.localeCompare(hacker2) === -1) {
   console.log("What?! You both have the same name?")
 }
 
+
 //Bonus 1
 let loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel faucibus tortor. Mauris sed lectus sed felis euismod pretium quis vel sem. Nulla aliquet mollis ex, sit amet sagittis est mattis nec. Vivamus sed leo tincidunt, efficitur purus a, tincidunt est. Donec tincidunt sem diam, non venenatis leo eleifend pretium. Aenean eleifend purus orci, vitae mattis nunc accumsan ac. Sed tempus tellus nec felis mollis fringilla. Aliquam rutrum eget tellus in porttitor.
-Suspendisse hendrerit eros vitae semper sagittis. Aliquam posuere tristique lacus at blandit. Integer ac elit lacus. Nam scelerisque feugiat metus eget gravida. Pellentesque eu finibus velit, sit amet dictum velit. Morbi gravida ligula eu est tempor tincidunt. Pellentesque et tempus nulla, ut eleifend ligula. Nulla facilisi. Praesent nec finibus est. Nunc neque turpis, lobortis ac pharetra eu, pulvinar eu leo. Nam aliquam faucibus maximus. Suspendisse fermentum ante nec orci consectetur, ac feugiat orci tincidunt.
-Vestibulum felis nibh, porttitor vitae velit a, interdum varius arcu. Proin ultricies magna vel erat lacinia, vel pulvinar dolor commodo. Sed mattis, nisl eget vestibulum imperdiet, diam ipsum porttitor diam, eget tempor velit ligula vel sem. Vestibulum id dignissim ipsum. Donec venenatis eu ipsum egestas tempor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque feugiat sem non enim sollicitudin rhoncus.`
-console.log(loremIpsum.split(' ').length)
-console.log(loremIpsum.includes("et"))
 
+Suspendisse hendrerit eros vitae semper sagittis. Aliquam posuere tristique lacus at blandit. Integer ac elit lacus. Nam scelerisque feugiat metus eget gravida. Pellentesque eu finibus velit, sit amet dictum velit. Morbi gravida ligula eu est tempor tincidunt. Pellentesque et tempus nulla, ut eleifend ligula. Nulla facilisi. Praesent nec finibus est. Nunc neque turpis, lobortis ac pharetra eu, pulvinar eu leo. Nam aliquam faucibus maximus. Suspendisse fermentum ante nec orci consectetur, ac feugiat orci tincidunt.
+
+Vestibulum felis nibh, porttitor vitae velit a, interdum varius arcu. Proin ultricies magna vel erat lacinia, vel pulvinar dolor commodo. Sed mattis, nisl eget vestibulum imperdiet, diam ipsum porttitor diam, eget tempor velit ligula vel sem. Vestibulum id dignissim ipsum. Donec venenatis eu ipsum egestas tempor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque feugiat sem non enim sollicitudin rhoncus.`
+//Make your program count the number of words in the string.
+console.log(loremIpsum.split(' ').length)
+//Make your program count the number of times the Latin word et appears.
 console.log(loremIpsum.split(' et ').length-1)
+
+
+//Bonus2
+let phraseToCheck = "No 'x' in Nixon"
+
+if (phraseToCheck.toUpperCase().replace(/[^a-zA-Z]/g, '') === phraseToCheck.split("").reverse().join("").toUpperCase().replace(/[^a-zA-Z]/g, '')) {
+  console.log(`The phrase '${phraseToCheck}' is a palindrome.`)
+} else {
+  console.log(`The phrase '${phraseToCheck}' is NOT a palindrome.`)
+}
